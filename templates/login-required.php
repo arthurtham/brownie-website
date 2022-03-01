c41a62768cd17e595341b54561d15cb0197425ae
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2022-03-01 01:57:51 -0800

    Triple Quotes Fix For Login Required

--- templates/login-required.php
+++ templates/login-required.php
@@ -8,24 +8,24 @@
     <li>Join the Turtle Pond Discord Server: <?php 
         echo check_guild_membership($guild_id) ? "Joined!" : <<<DISC
         Not yet joined! <a href="https://discord.gg/c8XNspY" target="_blank">Join the Turtle Pond!</a></iframe>;
-        DISC;
+DISC;
     ?></li>
     <li>Satisfy at least one role requirement: 
         <ul>
             <li>Red Shells (Subs): <?php 
                 echo check_roles([$sub_role_id]) ? "Subbed on Twitch!" : <<<DISC
                 Gain this role by subbing to Browntul on https://twitch.tv/browntulstar and linking your Twitch account to Discord.
-                DISC;
+DISC;
                 ?></li>
             <li>Stars (VIPs): <?php 
                 echo check_roles([$vip_role_id]) ? "You're special already!" : <<<DISC
                 Only for the special ones.
-                DISC;
+DISC;
                 ?></li>
             <li>Blue Shells (Mods): <?php 
                 echo check_roles([$mod_role_id]) ? "You're special already!" : <<<DISC
                 Only for my bodyguards.
-                DISC;
+DISC;
                 ?></li>
         </ul>
     </li>
