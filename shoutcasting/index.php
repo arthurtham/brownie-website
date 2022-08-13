e2543163d6ac5590ca91f91a70cb19fb25179414
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2022-08-12 22:41:16 -0700

    Update shoutcasting page to redirect to its own section

---
+++ shoutcasting/index.php
@@ -0,0 +1,97 @@
+<?php
+$dir = dirname(__DIR__, 1);
+$title = "BrowntulStar - Shoutcasting";
+require $dir . "/templates/header.php";
+?>
+<div class="container body-container" style="padding-top:50px;padding-bottom:100px">
+    <h1 class="text-center">Shoutcasting Portfolio</h1>
+    <p>I am an amateur play-by-play shoutcaster, with a recurring casting role for Wilson Chang's Chang Gang Tournaments, Rooyemi's VALORANT tournaments, and Change Thru Games' VALORANT and Tetris tournaments.</p>
+    <p>I offer <strong>play-by-play shoutcasting</strong> with puns, hype, and laughter for VALORANT, with flexibility for variety games like Mario Kart and Tetris.</p>
+    <p>On occasion, I host the Brownie-Toaster VALORANT Scrimmage Series on my Twitch channel to give newer shoutcasters and observers a shot at these great roles, while providing new tournament players a chance to play in a tournament environment.
+    <p>Below are some of my favorite highlights throughout my shoutcasting career, followed by a list of tournaments I've shoutcasted.</p>
+    <hr>
+    <p><center>
+
+    <h2>Highlights</h2>
+    <h5>Click the bars on the bottom to scroll</h5>
+    <div id="carouselVideos" class="carousel carousel-dark" data-bs-ride="carousel" data-bs-interval="false" style="min-height:600px">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselVideos" data-bs-slide-to="7" aria-label="Slide 8"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <blockquote class="twitter-tweet"><p lang="en" dir="ltr">It was a great time shoutcasting <a href="https://twitter.com/shygori?ref_src=twsrc%5Etfw">@shygori</a> &#39;s VALADIES tournament earlier this month! This clip, between Waku Waku and KIMBAO, is one of my favorites. <br><br>🎙️ <a href="https://twitter.com/browntulstar?ref_src=twsrc%5Etfw">@browntulstar</a> (pbp), <a href="https://twitter.com/AwesomeLioness1?ref_src=twsrc%5Etfw">@AwesomeLioness1</a> (analyst)<br>📷 <a href="https://twitter.com/shygori?ref_src=twsrc%5Etfw">@shygori</a> <br>🎮 <a href="https://twitter.com/PlayVALORANT?ref_src=twsrc%5Etfw">@PlayVALORANT</a> <a href="https://twitter.com/hashtag/VALORANT?src=hash&amp;ref_src=twsrc%5Etfw">#VALORANT</a><br>📺 <a href="https://t.co/wPIjswjh94">https://t.co/wPIjswjh94</a> <a href="https://t.co/G9004rSVi7">pic.twitter.com/G9004rSVi7</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1552196396442271744?ref_src=twsrc%5Etfw">July 27, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>            
+                <br/><br/>
+            </div>
+            <div class="carousel-item">
+                <p>Shoutcaster Showcase: Games 1 and 3</p>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/T09ec4OrCBg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br><br><a target="_blank" href="https://www.youtube.com/watch?v=4rRH1gWFBB0">Click here to watch the full vod</a>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet"><p lang="en" dir="ltr">I had a great time shoutcasting the Grand Finals of the CGT: Battle of the Streamers hosted by <a href="https://twitter.com/_WilsonChang?ref_src=twsrc%5Etfw">@_WilsonChang</a> and <a href="https://twitter.com/achauie?ref_src=twsrc%5Etfw">@achauie</a> . In this thread are a subset of my favorite clips from the final series.<br><br>PBP: <a href="https://twitter.com/browntulstar?ref_src=twsrc%5Etfw">@browntulstar</a> <br>Analysts: <a href="https://twitter.com/AwesomeLioness1?ref_src=twsrc%5Etfw">@AwesomeLioness1</a> and itsJustin<br>Observer: <a href="https://twitter.com/MrGeeelz?ref_src=twsrc%5Etfw">@MrGeeelz</a> <a href="https://t.co/pjz6EMHZZJ">https://t.co/pjz6EMHZZJ</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1542777571720302595?ref_src=twsrc%5Etfw">July 1, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <a target="_blank" href="https://twitter.com/browntulstar/status/1542777571720302595">Click here to watch all clips from this tournament</a>
+                <br/><br/><br/>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet"><p lang="en" dir="ltr">I had fun shoutcasting <a href="https://twitter.com/_WilsonChang?ref_src=twsrc%5Etfw">@_WilsonChang</a> CGT again. Lucky to be paired up with analyst <a href="https://twitter.com/AwesomeLioness1?ref_src=twsrc%5Etfw">@AwesomeLioness1</a> and the legendary <a href="https://twitter.com/Zaeter_?ref_src=twsrc%5Etfw">@Zaeter_</a>. <a href="https://twitter.com/heine1004?ref_src=twsrc%5Etfw">@heine1004</a> &#39;s shoutcasting debut was spectacular! Hope we can do this again.<br><br>Thread: a sample of casting clips with me paired with each caster. <a href="https://t.co/OHYB33izbs">https://t.co/OHYB33izbs</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1526335377287413761?ref_src=twsrc%5Etfw">May 16, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <a target="_blank" href="https://twitter.com/browntulstar/status/1526335377287413761">Click here to watch all clips from this tournament</a>
+                <br/><br/><br/>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">Watch the Instigators take on TLC&#39;s lineups and crossfire on Haven. Who will clutch it out in overtime?<br><br>🎙️ <a href="https://twitter.com/browntulstar?ref_src=twsrc%5Etfw">@browntulstar</a> and <a href="https://twitter.com/AwesomeLioness1?ref_src=twsrc%5Etfw">@AwesomeLioness1</a> <br>📷 <a href="https://twitter.com/Rooyemi_?ref_src=twsrc%5Etfw">@Rooyemi_</a> <br>🎮 <a href="https://twitter.com/PlayVALORANT?ref_src=twsrc%5Etfw">@PlayVALORANT</a> <a href="https://twitter.com/hashtag/VALORANT?src=hash&amp;ref_src=twsrc%5Etfw">#VALORANT</a> <br>📺 <a href="https://t.co/OlwR6TrdPF">https://t.co/OlwR6TrdPF</a><a href="https://twitter.com/hashtag/shoutcasting?src=hash&amp;ref_src=twsrc%5Etfw">#shoutcasting</a> <a href="https://twitter.com/hashtag/tournament?src=hash&amp;ref_src=twsrc%5Etfw">#tournament</a> <a href="https://twitter.com/hashtag/clutch?src=hash&amp;ref_src=twsrc%5Etfw">#clutch</a> <a href="https://t.co/RV0S58htIa">pic.twitter.com/RV0S58htIa</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1498706260299948036?ref_src=twsrc%5Etfw">March 1, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <br/><br/>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">Nothing like <a href="https://twitter.com/edmvndee?ref_src=twsrc%5Etfw">@edmvndee</a> getting the ace to win <a href="https://twitter.com/_WilsonChang?ref_src=twsrc%5Etfw">@_WilsonChang</a> &#39;s CGT Friendship Games! The Tour de Force and the Headhunter were all he needed.<br><br>🎙️<a href="https://twitter.com/browntulstar?ref_src=twsrc%5Etfw">@browntulstar</a> and <a href="https://twitter.com/Kayvian_koi?ref_src=twsrc%5Etfw">@Kayvian_koi</a> on the call.<a href="https://twitter.com/hashtag/VALORANT?src=hash&amp;ref_src=twsrc%5Etfw">#VALORANT</a> <a href="https://twitter.com/hashtag/chamber?src=hash&amp;ref_src=twsrc%5Etfw">#chamber</a> <a href="https://twitter.com/hashtag/ace?src=hash&amp;ref_src=twsrc%5Etfw">#ace</a> <a href="https://twitter.com/hashtag/tournament?src=hash&amp;ref_src=twsrc%5Etfw">#tournament</a> <br>Stream: <a href="https://t.co/wiaMwfTPZc">https://t.co/wiaMwfTPZc</a><br>Game: <a href="https://twitter.com/PlayVALORANT?ref_src=twsrc%5Etfw">@PlayVALORANT</a> <a href="https://t.co/H5YAIg0KmK">pic.twitter.com/H5YAIg0KmK</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1495998253237735424?ref_src=twsrc%5Etfw">February 22, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <br/><br/>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">Looking back at the Happy Roolidays! <a href="https://twitter.com/hashtag/VALORANT?src=hash&amp;ref_src=twsrc%5Etfw">#VALORANT</a> Tourney. Shoutcasting it was a fun learning experience and it&#39;s enhanced by the analysis of the one and only <a href="https://twitter.com/AwesomeLioness1?ref_src=twsrc%5Etfw">@AwesomeLioness1</a> . Thanks to <a href="https://twitter.com/Enlightgg?ref_src=twsrc%5Etfw">@Enlightgg</a> for bringing people with similar interests together!<br><br>(From TTV/Rooyemi <a href="https://twitter.com/Rooyemi_?ref_src=twsrc%5Etfw">@Rooyemi_</a> ) <a href="https://t.co/brDb3Bl6u6">pic.twitter.com/brDb3Bl6u6</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1474640366036930560?ref_src=twsrc%5Etfw">December 25, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <br/><br/>
+            </div>
+            <div class="carousel-item">
+                <blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">I miss shoutcasting :(<br>Happy 1-year anniversary of shoutcasting Tetris for <a href="https://twitter.com/ChangeThruGames?ref_src=twsrc%5Etfw">@ChangeThruGames</a> 🥳 <a href="https://t.co/mOELNtibxe">pic.twitter.com/mOELNtibxe</a></p>&mdash; Browntul (@browntulstar) <a href="https://twitter.com/browntulstar/status/1400271125620281344?ref_src=twsrc%5Etfw">June 3, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <br/><br/>
+            </div>
+            
+        </div>
+    </div>
+
+    </center></p>
+    <hr>
+    <p>Here's a list of tournaments I've shoutcasted:</p>
+    <ul>
+        <li><strong>Shygori's VALADIES All Girls Tourney</strong><br/>(July 2022) - Play-by-Play, Color Analyst<br/>
+        <a href="https://twitch.tv/shygori">https://twitch.tv/shygori</a></li>
+        <li><strong>SOGIMA Summer Ace</strong><br/>(July 2022) - Play-by-Play, Color Analyst<br/>
+        <a href="https://twitch.tv/sooomoza">https://twitch.tv/sooomoza</a></li>
+        <li><strong>CGT: Battle of the Streamers</strong><br/>(June 2022) - Play-by-Play<br/>
+        <a href="https://twitch.tv/wilsonchang">https://twitch.tv/wilsonchang</a></li>
+        <li><strong>Brownie-Chicks VALORANT Scrimmage Series</strong><br/>(June 2022) - Play-by-Play<br/>
+        <a href="https://twitch.tv/browntulstar">https://twitch.tv/browntulstar</a></li>
+        <li><strong>CGT: Battle of the Coasts</strong><br/>(May 2022) - Play-by-Play, Color Analyst<br/>
+        <a href="https://twitch.tv/wilsonchang">https://twitch.tv/wilsonchang</a></li>
+        <li><strong>ValorANT Tournament (UCI)</strong><br/>(March 2022) - Play-by-Play<br/>
+        <a href="https://twitch.tv/rooyemi">https://twitch.tv/rooyemi</a></li>
+        <li><strong>Happy Valrootines VALORANT Tournament</strong><br/>(February 2022) - Play-by-Play<br/>
+        <a href="https://twitch.tv/rooyemi">https://twitch.tv/rooyemi</a></li>
+        <li><strong>CGT: Friendship Games</strong><br/>(February 2022) - Play-by-Play<br/>
+        <a href="https://twitch.tv/wilsonchang">https://twitch.tv/wilsonchang</a></li>
+        <li><strong>Happy Roolidays VALORANT Tournament</strong><br/>(December 2021) - Play-by-Play<br/>
+        <a href="https://twitch.tv/rooyemi">https://twitch.tv/rooyemi</a></li>
+        <li><strong>Change Thru Games (UCI): Tetris and VALORANT</strong><br/>(2020-present) - Play-by-Play<br/>
+        <a href="https://twitch.tv/changethrugames">https://twitch.tv/changethrugames</a></li> 
+        <li><strong>O Snap Its a VALORANT Tournament by osnapitskat</strong><br/>(August 2020, July 2021, November 2021, June 2022) - Shoutcaster<br/>
+        <a href="https://twitch.tv/osnapitskat">https://twitch.tv/osnapitskat</a></li>
+    </ul>
+    <p>If you are interested in booking me to shoutcast your games, please email me by clicking on the <strong>Contact</strong> button on the navigation bar on the top of this page!</p>
+</div>
+<?php require $dir . "/templates/footer.php" ?>
