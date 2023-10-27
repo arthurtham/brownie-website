a8116121745408c983c9c8890de4e9266782c9c8
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2023-10-26 22:15:11 -0700

    Added 404 Page

---
+++ error/404.php
@@ -0,0 +1,11 @@
+<?php
+$dir = dirname(__DIR__, 1);
+$title = "BrowntulStar - Error"; 
+require $dir . "/templates/header.php";
+?>
+
+<div class="container body-container" style="padding-top:50px;padding-bottom:100px">
+    <h1 class="text-center">404 Error</h1>
+    <p>Uh oh! Looks like this page doesn't exist. Uninspirational!</p>
+</div>
+<?php require $dir . "/templates/footer.php" ?>
