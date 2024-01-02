18c719adeb221dd8c3764282939de528133c6341
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-01-01 21:35:32 -0800

    Add a script that deletes uploaded videos to the Cloudinary lab after some time have passed
    - Requires a cron job to run this script on a periodic timeline

---
+++ includes/cloudinarydeleteaftertime.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once "cloudinary.env.php";
+use Cloudinary\Api\Search\SearchApi;
+use Cloudinary\Api\Admin\AdminApi;
+
+
+$result = (new SearchApi())->expression(
+    $CLOUDINARY_DELETE_EXPRESSION
+    )
+    ->sortBy('uploaded_at','desc')
+    ->execute();
+
+$uploaded_at = array();
+$public_ids_to_delete = array();
+foreach ($result["resources"] as $rid => $resource) {
+    array_push($uploaded_at, array(
+        "upload_date" => $resource["uploaded_at"],
+        "public_id" => $resource["public_id"],
+        "asset_id" => $resource["asset_id"]
+    ));
+    array_push($public_ids_to_delete, $resource["public_id"]);
+}
+
+$admin_api = new AdminApi();
+
+foreach (array_chunk($public_ids_to_delete, 100) as $public_ids_to_delete_chunk) {
+    echo "<pre>";
+    print_r($public_ids_to_delete_chunk);
+    echo "</pre>";
+    echo "<pre>";
+    print_r($admin_api -> deleteAssets($public_ids_to_delete_chunk, array(
+        "invalidate" => true,
+        "resource_type" => "video"
+    )));
+    echo "</pre>";
+}
+
+echo "<h1>List of deleted assets:</h1><pre>";
+print_r($uploaded_at);
+echo "</pre>";
+
+
+?>
