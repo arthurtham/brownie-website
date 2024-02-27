f2d60ffdf58ee0214668a7830b5cbad7c92ed67d
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-02-27 15:37:26 -0800

    Hotfix to allow mysql configuration

--- includes/mysql.php
+++ includes/mysql.php
@@ -4,7 +4,7 @@ require_once(".env.php");
 
 $servername = $_environmental_variables["servername"];
 $username = $_environmental_variables["username"];
-$password = "";//$_environmental_variables["password"];
+$password = $_environmental_variables["password"];
 $dbname = $_environmental_variables["dbname"];
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
