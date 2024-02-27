d24a0dc8ef60c8bc278bda8edaf9ecbac1fbdca5
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-02-27 04:17:07 -0800

    Hotfix to force center alignment of home page on page load
    Also adds a fade-in effect to the home page's center block

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ BADAUTH;
 EXPIRED;
 	}
 	?>
-	<div id="center-block" class="d-flex justify-content-center">
+	<div id="center-block" class="d-flex align-items-center justify-content-center">
 		<center><img src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_400/com.browntulstar/img/browntulstar-logo-v2-large.webp" style="width:100%;max-width:500px" />
 		<div class="box bg-gradient shadow rounded" style="background-color: rgba(255,255,255,0.2); padding: 20px; width:100%; max-width:370px">
 			<span class="d-flex flex-row gap-1" style="text-align: center; padding-bottom:6px">
