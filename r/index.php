30ca8f82bae6ff0401a3048b89ff7594a806d882
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-02-28 04:49:04 -0800

    Improvements to links systems
    - Redirect link system now tracks link clicks and throws a 404 error if a link is not found.
    - Footer links now directly link to webpages, except for the Discord server link which points to the redirect system.
    - Add extra relevant links to footer.

--- r/index.php
+++ r/index.php
@@ -3,17 +3,23 @@ $dir = dirname(__DIR__, 1);
 require_once($dir . "/includes/mysql.php");
 
 if (!isset($_GET["shortcode"])) {
-    header("Location: /");
+    //http_response_code(404);
+    header("Location: /error/404.php", true, 404);
+    die();
 }
 
 $sql = "SELECT * FROM shortlinks WHERE shortcode = \"".mysqli_real_escape_string($conn, $_GET["shortcode"])."\" AND available = 1 LIMIT 1";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($item = $result->fetch_assoc()) {
-        header("Location: ".$item["fulllink"], TRUE, 301);
+        $sql_update = "UPDATE shortlinks SET hits = IFNULL(hits, 0) + 1 WHERE id = ".$item["id"].";";
+        $result_update = $conn->query($sql_update);
+        header("Location: ".$item["fulllink"], true, 301);
     }
 } else {
-    header("Location: /");
+    //http_response_code(404);
+    header("Location: /error/404.php", true, 404);
+    die();
 }
 
 
--- templates/footer.php
+++ templates/footer.php
@@ -9,48 +9,63 @@ if (!isset($_FOOTER_ENABLE) || $_FOOTER_ENABLE == true) {
 
 <footer class="d-flex flex-column w-100 justify-content-center align-items-center border-top bg-light shadow" style="{$_footer_style}">
     <div class="row w-100">
-        <div class="col-lg-4">
-            <ul class="nav justify-content-center list-unstyled d-flex">
+        <div class="col-lg-5">
+            <ul class="nav justify-content-center list-unstyled d-flex" style="font-size:20px">
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;color:#000" href="https://twitch.tv/browntulstar" target="_blank">
+                    <a style="text-decoration: none;color:#000" href="https://twitch.tv/browntulstar" target="_blank">
                         <i class="fab fa-twitch"></i>
                     </a> 
                 </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;color:#000" href="https://x.com/browntulstar" target="_blank">
+                    <a style="text-decoration: none;color:#000" href="https://x.com/browntulstar" target="_blank">
                         <i class="fa-brands fa-x-twitter"></i>
                     </a>
                 </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;color:#000" href="/youtube" target="_blank">
+                    <a style="text-decoration: none;color:#000" href="https://instagram.com/browntulstar" target="_blank">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="ms-2">
+                    <a style="text-decoration: none;color:#000" href="https://youtube.com/@browntulstar" target="_blank">
                         <i class="fa-brands fa-youtube"></i>
                     </a>
                 </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;color:#000" href="/tiktok" target="_blank">
+                    <a style="text-decoration: none;color:#000" href="https://tiktok.com/@browntulstar" target="_blank">
                         <i class="fa-brands fa-tiktok"></i>
                     </a>
                 </li>     
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;;color:#000" href="/discord" target="_blank">
+                    <a style="text-decoration: none;color:#000" href="https://browntulstar.com/r/discord" target="_blank">
                         <i class="fab fa-discord"></i>
                     </a> 
-                </li>  
+                </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:24px;" href="https://ko-fi.com/browntulstar" target="_blank">
-                        <img src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_24/com.browntulstar/img/platform-kofi-support-dark.png">
+                    <a style="text-decoration: none;" href="https://throne.me/browntulstar" target="_blank">
+                        <img class="rounded" style="height:20px;margin-top:-4px" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,w_48,h_48,ar_1:1/com.browntulstar/img/platform-throne.webp">
                     </a> 
                 </li> 
+                <li class="ms-2">
+                    <a style="text-decoration: none;" href="https://ko-fi.com/browntulstar" target="_blank">
+                        <img style="height:20px;margin-top:-4px" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_48/com.browntulstar/img/platform-kofi.webp">
+                    </a> 
+                </li> 
+                <li class="ms-2">
+                    <a style="text-decoration: none;" href="https://brownieval.browntulstar.com" target="_blank">
+                        <img class="rounded" style="height:20px;margin-top:-4px" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,w_48,h_48,ar_1:1/v1705971391/com.browntulstar/img/brownieval-logo-v1.webp">
+                    </a> 
+                </li>
             </ul>
         </div>
-        <div class="col-lg-4">
-            <ul class="nav justify-content-center list-unstyled d-flex">
-            <span class="text-muted">© 2024 BrowntulStar</span>
+        <div class="col-lg-2">
+            <ul class="nav justify-content-center list-unstyled d-flex" style="margin-top:4px">
+            <small><span class="text-muted">© 2024 BrowntulStar</span></small>
         </div>
-        <div class="col-lg-4">
-            <ul class="nav justify-content-center list-unstyled d-flex">
+        <div class="col-lg-5">
+            <ul class="nav justify-content-center list-unstyled d-flex" style="margin-top:4px;font-size:14px">
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:16px" href="https://browntulstar.com/r/links" target="_blank">
+                    <a style="text-decoration: none;" href="https://browntulstar.com/r/links" target="_blank">
                         More Links
                     </a>
                 </li>
@@ -58,7 +73,7 @@ if (!isset($_FOOTER_ENABLE) || $_FOOTER_ENABLE == true) {
                     |
                 </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:16px" href="/privacy">
+                    <a style="text-decoration: none;" href="/privacy">
                         Privacy Policy
                     </a> 
                 </li>
@@ -66,7 +81,7 @@ if (!isset($_FOOTER_ENABLE) || $_FOOTER_ENABLE == true) {
                     |
                 </li>
                 <li class="ms-2">
-                    <a style="text-decoration: none;font-size:16px" href="mailto:browntulstar@browntulstar.com">
+                    <a style="text-decoration: none;" href="mailto:browntulstar@browntulstar.com">
                         Contact
                     </a> 
                 </li>
