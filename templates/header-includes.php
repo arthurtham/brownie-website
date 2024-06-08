f4e916be82248815179d606f3981cbfb26b9348c
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-06-08 02:26:14 -0700

    Hotfix style.css and css cache reset

--- templates/header-includes.php
+++ templates/header-includes.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta charset="utf-8">
 <title><?php echo $title ?></title>
-<link rel="stylesheet" href="/assets/css/style.css?v=2024-6-4">
+<link rel="stylesheet" href="/assets/css/style.css?v=2024-6-8">
 <script src="https://kit.fontawesome.com/7f5f717705.js" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <meta name="og:title" content="<?php echo $title ?>">
