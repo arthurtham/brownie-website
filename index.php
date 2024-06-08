3d23b4ede9b8b825023ecbd3ed181f75afc2b2cd
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2024-06-08 03:19:33 -0700

    Added organizations to home page
    - Re-added orgs section from about page to new home page.

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ require $dir . "/templates/header.php"
 				</div>
 				<div class="row">
 					<div class="col-md-3 mb-2">
-						<div class="card" style="width: 100%; height:100%">
+						<div class="card w-100 h-100">
 							<img src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/games-mk8dx.webp" class="card-img-top" alt="mario kart 8 deluxe" style="object-fit: cover; object-position: left; height:200px">
 							<div class="card-body">
 								<h5 class="card-title  text-center">Mario Kart 8 Deluxe</h5>
@@ -114,7 +114,7 @@ require $dir . "/templates/header.php"
 						</div>
 					</div>
 					<div class="col-md-3 mb-2">
-						<div class="card" style="width: 100%; height:100%">
+						<div class="card w-100 h-100">
 							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/games-krr.webp" class="card-img-top" alt="KRR" style="object-fit: cover;height:200px">
 							<div class="card-body">
 								<h5 class="card-title  text-center">KartRider: Drift</h5>
@@ -122,7 +122,7 @@ require $dir . "/templates/header.php"
 						</div>
 					</div>
 					<div class="col-md-3 mb-2">
-						<div class="card" style="width: 100%; height:100%">
+						<div class="card w-100 h-100">
 							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/games-honkaistarrail.webp" class="card-img-top" alt="qingque honkai star rail" style="object-fit: cover;height:200px">
 							<div class="card-body">
 								<h5 class="card-title  text-center">Honkai: Star Rail</h5>
@@ -130,7 +130,7 @@ require $dir . "/templates/header.php"
 						</div>
 					</div>
 					<div class="col-md-3 mb-2">
-						<div class="card" style="width: 100%; height:100%">
+						<div class="card w-100 h-100">
 							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/games-valorant-kj.webp" class="card-img-top" alt="valorant killjoy" style="object-fit: cover;height:200px">
 							<div class="card-body">
 								<h5 class="card-title  text-center">VALORANT</h5>
@@ -138,10 +138,66 @@ require $dir . "/templates/header.php"
 						</div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<hr />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center text-white">Organizations I'm In</h1>
+					</div>
+				</div>
+				<div class="row mb-2">
+					<div class="col-lg-3 mb-2">
+						<div class="card w-100 h-100">
+							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,b_gen_fill,w_430,h_261/com.browntulstar/img/brownieval-logo-v1.webp" class="card-img-top" alt="okimi logo" style="padding: 20px;object-fit: contain;height:200px">
+							<div class="card-body">
+								<h5 class="card-title  text-center">#BrownieVAL</h5>
+								<h6 class="text-center">Owner</h6>
+								<p style="text-align:center"><a class="btn btn-success w-100" style="max-width:300px" href="https://brownieval.browntulstar.com/" target="_blank">Website</a></p>
+								<p style="text-align:center"></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 mb-2">
+						<div class="card w-100 h-100">
+							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/team-kazoku.webp" class="card-img-top" alt="kazoku logo" style="padding: 20px;object-fit: contain;height:200px">
+							<div class="card-body">
+								<h5 class="card-title  text-center">Kazoku</h5>
+								<h6 class="text-center">Events Coordinator</h6>
+								<p style="text-align:center"><a class="btn btn-success w-100" style="max-width:300px" href="https://kazoteam.carrd.co/" target="_blank">Website</a></p>
+								<p style="text-align:center"></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 mb-2">
+						<div class="card w-100 h-100">
+							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,h_200/com.browntulstar/img/team-comfycafelogo.webp" class="card-img-top" alt="okimi logo" style="padding: 20px;object-fit: contain;height:200px">
+							<div class="card-body">
+								<h5 class="card-title  text-center">Okimi Cafe</h5>
+								<h6 class="text-center">Co-founder and Co-manager Emeritus</h6>
+								<p style="text-align:center"><a class="btn btn-success w-100" style="max-width:300px" href="https://okimicafe.crd.co/" target="_blank">Website</a></p>
+								<p style="text-align:center"></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 mb-2">
+						<div class="card w-100 h-100">
+							<img loading="lazy" src="https://res.cloudinary.com/browntulstar/image/private/c_pad,b_gen_fill,w_430,h_200/com.browntulstar/img/team-rdv.webp" class="card-img-top" alt="okimi logo" style="padding: 20px;object-fit: contain;height:200px">
+							<div class="card-body">
+								<h5 class="card-title  text-center">Rendezvous Valorant</h5>
+								<h6 class="text-center">Lead Play-by-play Caster</h6>
+								<p style="text-align:center"><a class="btn btn-success w-100" style="max-width:300px" href="https://twitter.com/rendezvousval" target="_blank">Website</a></p>
+								<p style="text-align:center"></p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div id="home-section-watch" class="row py-5 d-flex align-items-center justify-content-center home-div-row brownie-red" style="padding-bottom:100px !important">
+	<div id="home-section-watch" class="row py-5 d-flex align-items-center justify-content-center home-div-row brownie-red" style="padding-bottom:200px !important; min-height:fit-content !important">
 		<div class="col-lg-12 d-flex justify-content-center home-div-col">
 			<div class="container">
 				<div class="row">
