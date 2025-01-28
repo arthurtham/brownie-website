e798e0109487098985bcfc79d72b6d63b0ba35fc
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-01-28 03:16:07 -0800

    Add Concentration Training app folder
    - Add new app called "Concentration Training" whose files are maintained in a different repo.
    - Add .htaccess rules to enable Unity gzip decompression

---
+++ app/concentration/index.php
@@ -0,0 +1,12 @@
+<?php
+$dir = dirname(__DIR__, 2);
+$title = "Turtle Pond - Concentration Training";
+require $dir . "/templates/header.php";
+
+?>
+<div style="padding-top:56px">
+<iframe src="game.html" style="width: 100%; height: calc(100vh - 56px); border: none;"></iframe>
+</div>
+<?php
+$_FOOTER_HOME = true;
+require $dir . "/templates/footer.php";
