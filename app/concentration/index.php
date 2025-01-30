5ccc11f4c6aa1f95a10b5b5c6693bc8194f94495
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-01-30 01:24:13 -0800

    Force cache reset for Cash Mountain and Concentration iframes

--- app/concentration/index.php
+++ app/concentration/index.php
@@ -4,9 +4,17 @@ $title = "Turtle Pond - Concentration Training";
 require $dir . "/templates/header.php";
 
 ?>
-<div style="padding-top:56px">
-<iframe src="./game/game.html" style="width: 100%; height: calc(100vh - 56px); border: none;"></iframe>
+<div style="padding-top:56px" name="game-div" id="game-div">
 </div>
+<script>
+const iframe = document.createElement('iframe');
+iframe.src = "./game/game.html?timestamp="+new Date().getTime();
+iframe.style.width = "100%";
+iframe.style.height = "calc(100vh - 56px)"; 
+iframe.style.border = "none";
+document.getElementById("game-div").appendChild(iframe);
+
+</script>
 <?php
 $_FOOTER_HOME = true;
 require $dir . "/templates/footer.php";
