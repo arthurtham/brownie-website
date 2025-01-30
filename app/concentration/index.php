5cb34b1cc19f577390c3c9c924d071e15df6dcce
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-01-29 19:37:06 -0800

    Change game directories for existing game modules

--- app/concentration/index.php
+++ app/concentration/index.php
@@ -5,7 +5,7 @@ require $dir . "/templates/header.php";
 
 ?>
 <div style="padding-top:56px">
-<iframe src="game.html" style="width: 100%; height: calc(100vh - 56px); border: none;"></iframe>
+<iframe src="./game/game.html" style="width: 100%; height: calc(100vh - 56px); border: none;"></iframe>
 </div>
 <?php
 $_FOOTER_HOME = true;
