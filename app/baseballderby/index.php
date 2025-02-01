a5e8bc40a427f7ee25c3488983de394a390526db
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-02-01 03:49:00 -0800

    Add module for Baseball Derby game

---
+++ app/baseballderby/index.php
@@ -0,0 +1,20 @@
+<?php
+$dir = dirname(__DIR__, 2);
+$title = "BrowntulStar - Guzman HR Derby";
+require $dir . "/templates/header.php";
+
+?>
+<div style="padding-top:56px" name="game-div" id="game-div">
+</div>
+<script>
+const iframe = document.createElement('iframe');
+iframe.src = "./game/game.html?timestamp="+new Date().getTime();
+iframe.style.width = "100%";
+iframe.style.height = "calc(100vh - 56px)"; 
+iframe.style.border = "none";
+document.getElementById("game-div").appendChild(iframe);
+
+</script>
+<?php
+$_FOOTER_HOME = true;
+require $dir . "/templates/footer.php";
