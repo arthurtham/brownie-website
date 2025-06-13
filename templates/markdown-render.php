e70af27943c9712d0836704044c3d6ae8029d190
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-06-13 00:49:57 -0700

    Fix markdown renderer not rendering media properly
    - Explicitly allow videos in the HTML Purifier Config
    - Fix width for images and videos if they exceed the browser width

--- templates/markdown-render.php
+++ templates/markdown-render.php
@@ -3,8 +3,40 @@ if (!isset($embed_contents)) {
     $embed_contents = "";
 }
 $_mdconfig = HTMLPurifier_Config::createDefault();
-// $_mdconfig->set('HTML.Allowed', 'a[href],b,strong,i,em,p,br,ul,ol,li,blockquote,code,pre,table,thead,tbody,tr,th,td');
+$_mdconfig->set('HTML.DefinitionID', 'com-browntulstar-markdown-video-support');
+$_mdconfig->set('HTML.DefinitionRev', 1);
 $_mdconfig->set('HTML.SafeScripting', array("https://platform.twitter.com/widgets.js"));
 $_mdconfig->set('HTML.SafeIframe', true);
 $_mdconfig->set('URI.SafeIframeRegexp', '%^(https?:)?//(www.youtube.com/|youtube.com/|youtu.be/)%');
+
+if ($def = $_mdconfig->maybeGetRawHTMLDefinition()) {
+    $def->addElement(
+        'video',
+        'Block',
+        'Flow', // Simpler and safe content model
+        'Common',
+        [
+            'src' => 'URI',
+            'controls' => 'Bool',
+            'width' => 'Length',
+            'height' => 'Length',
+            'poster' => 'URI',
+            'preload' => 'Enum#auto,metadata,none',
+            'autoplay' => 'Bool',
+            'loop' => 'Bool',
+            'muted' => 'Bool'
+        ]
+    );
+    $def->addElement(
+        'source',
+        'Inline',
+        'Empty',
+        'Common',
+        [
+            'src' => 'URI',
+            'type' => 'Text'
+        ]
+    );
+}
+
 echo (new HTMLPurifier($_mdconfig))->purify(Parsedown::instance()->setBreaksEnabled(true)->text($embed_contents));
