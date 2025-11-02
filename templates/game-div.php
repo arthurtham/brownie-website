c4e3f20e59be139b4b43341e5c4832b1f576debc
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2025-11-02 01:05:00 -0700

    Hotfix game div template due to reused variables

--- templates/game-div.php
+++ templates/game-div.php
@@ -1,6 +1,7 @@
 <?php 
+$_game_div_dir = dirname(__DIR__, 1);
 $_disallow_navbar = true;
-require $dir . "/templates/header.php";
+require $_game_div_dir . "/templates/header.php";
 if (!isset($_game_src)) {
     $_game_src = "./game/game.html";
 }
@@ -10,10 +11,10 @@ if (!isset($_game_src)) {
 <script>
 const iframe = document.createElement('iframe');
 iframe.className = "game-div-iframe";
-iframe.src = "<?=$_game_src; ?>?ts="+new Date().getTime()+"&ref="+document.referrer;
+iframe.src = "<?=$_game_src; ?>?ts="+new Date().getTime();
 document.getElementById("game-div").appendChild(iframe);
 </script>
 
 <?php
 $_FOOTER_APPS = true;
-require $dir . "/templates/footer.php";
+require $_game_div_dir . "/templates/footer.php";
